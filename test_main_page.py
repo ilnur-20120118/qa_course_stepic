7c6b5840de0b2224fdb50ff4dad351ea3fae715e
ilnur <ilnur.i.i.1990@gmail.com>
2022-02-13 16:27:58 +0400

    one more chang

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -12,4 +13,3 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
     
-     
