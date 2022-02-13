c7c993f9df63c79b58e8f8c9fb02a4a5352efa04
ilnur <ilnur.i.i.1990@gmail.com>
2022-02-13 18:58:52 +0400

    add one file and other modifided

---
+++ pages/login_page.py
@@ -0,0 +1,28 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        try:
+            login_url = browser.current_url()
+            if "login" in login_url:
+                assert True
+            else:
+                assert False, "login not in login_url"
+        except Exception:
+            assert False, "Error, хз что пошло не так"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "not login form"
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "not register form"
+    
