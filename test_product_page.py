47e7801ba349bd2297d290d57d1b02b68a469482
ilnur <ilnur.i.i.1990@gmail.com>
2022-03-25 16:14:28 +0400

    add new fun

--- test_product_page.py
+++ test_product_page.py
@@ -3,16 +3,18 @@ import time
 import pytest
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-     pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+link = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+        pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+
+@pytest.mark.parametrize('link', link)
 def test_guest_can_add_product_to_basket(browser, link):
     link = f"{link}"
     product_page = ProductPage(browser, link)
@@ -22,8 +24,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.solve_quiz_and_get_code()
     product_page.examination_correct_msg()
     product_page.examination_correct_price_in_basket()
-
-
+    product_page.should_not_disappear_msg()
 
 """
 Измените методы проверки таким образом, чтобы они принимали как аргумент название товара и цену товара.
@@ -31,4 +32,43 @@ def test_guest_can_add_product_to_basket(browser, link):
 Сделайте такой же метод для цены.
 Теперь проверяйте, что название товара в сообщении совпадает с заголовком товара.
 """
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    """
+    Открываем страницу товара
+    Добавляем товар в корзину
+    Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    """
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+    product_page.add_to_shoping_cart()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    """
+    Открываем страницу товара
+    Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    """
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    """
+    Открываем страницу товара
+    Добавляем товар в корзину
+    Проверяем, что нет сообщения об успехе с помощью is_disappeared
+    """
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+    product_page.add_to_shoping_cart()
+    product_page.should_disappear_msg()
+
 
